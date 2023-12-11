55e4b05e2f9fb6fd8e10da479474e68c05152de5
suleymankayir <suleymantalhakayir@gmail.com>
2023-12-11 11:19:03 +0300

    Daire Dilimi Alani Hesaplama commit

--- Taksimetre/src/Main.java
+++ Taksimetre/src/Main.java
@@ -4,13 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        /*
-        Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
-        Taksimetre KM başına 2.20 TL tutmaktadır.
-        Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
-        Taksimetre açılış ücreti 10 TL'dir.
-         */
-
         int acilisUcreti = 10;
         double kmBasiUcret = 2.20;
         int minTutar = 20;
