70342313e61203407a0d8679bedef1706e9001ab
suleymankayir <suleymantalhakayir@gmail.com>
2023-12-12 09:48:01 +0300

    Hava Sicakligina Gore Etkinlik Onerme commit

--- Gecme Notu/src/Main.java
+++ Gecme Notu/src/Main.java
@@ -3,44 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         int mat, fizik, turkce, kimya, muzik;
-        int ortalama = 0;
+        int toplamNot = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Matematik notunu giriniz: ");
         mat = scanner.nextInt();
         if (mat >= 0 && mat <= 100) {
-            ortalama += mat;
+            toplamNot += mat;
         }
 
         System.out.print("Fizik notunu giriniz: ");
         fizik = scanner.nextInt();
         if (fizik >= 0 && fizik <= 100) {
-            ortalama += fizik;
+            toplamNot += fizik;
         }
 
         System.out.print("Turkce notunu giriniz: ");
         turkce = scanner.nextInt();
         if (turkce >= 0 && turkce <= 100) {
-            ortalama += turkce;
+            toplamNot += turkce;
         }
 
         System.out.print("Kimya notunu giriniz: ");
         kimya = scanner.nextInt();
         if (kimya >= 0 && kimya <= 100) {
-            ortalama += kimya;
+            toplamNot += kimya;
         }
 
         System.out.print("Muzik notunu giriniz: ");
         muzik = scanner.nextInt();
         if (muzik >= 0 && muzik <= 100) {
-            ortalama += muzik;
+            toplamNot += muzik;
         }
 
-        System.out.println("Not Ortalamaniz = " + (ortalama/5));
-
-
-
+        int ortalama = toplamNot/5;
+        if (ortalama > 55){
+            System.out.println("Tebrikler geçtniz");
+        } else {
+            System.out.println("Notunuz: " + ortalama + " seneye tekrar deneyiniz");
+        }
     }
 }
