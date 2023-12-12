5bb911bff46ae445a3c5fb7331ace79439a6d0be
suleymankayir <suleymantalhakayir@gmail.com>
2023-12-13 00:29:46 +0300

    3 ve 4 bolunenler ortalama commit

--- Cift Sayi Bulma/src/Main.java
+++ Cift Sayi Bulma/src/Main.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int ort = 0,toplam=0,sayi = 0;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lütfen bir sayi giriniz");
         int input = scanner.nextInt();
 
         for (int i = 1; i <= input; i++) {
-            if (i % 2 == 0){
-                System.out.println(i);
+            if ((i % 3 == 0) && (i % 4 == 0)) {
+                ++sayi;
+                toplam += i;
+                ort = (toplam/sayi);
+
             }
 
         }
+        System.out.println(ort);
     }
 }
