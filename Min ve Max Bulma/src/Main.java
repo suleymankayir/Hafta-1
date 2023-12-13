2f7b59e5aa989a590cf0d5bfde5b9ad6afa6c264
suleymankayir <suleymantalhakayir@gmail.com>
2023-12-13 19:46:06 +0300

    Min ve Max Bulma commit

--- Min ve Max Bulma/src/Main.java
+++ Min ve Max Bulma/src/Main.java
@@ -3,13 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         int enBuyuk = Integer.MIN_VALUE;
         int enKucuk = Integer.MAX_VALUE;
-        /*
-        Java ile klavyeden girilen N tane sayma sayısından
-        en büyük ve en küçük sayıları bulan ve bu sayıları ekrana yazan programı yazın.
-         */
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lütfen kac sayi gireceginizi belirtiniz");
